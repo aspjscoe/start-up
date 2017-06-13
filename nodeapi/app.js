eb443f6447d470927676c2d85a97d82331f97090
unknown <aspjscoe@gmail.com>
2017-06-13 12:59:04 +0530

    post-api

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -6,27 +6,19 @@ var fs = require("fs");
 
 app.get('/users', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-   res.end( data );
+   res.send( data );
    });
 })
-
-
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
-
 app.post('/addUser', function (req, res) {
    // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
+   fs.readFile( __dirname + "/" + "addUser.json", 'utf8', function (err, data) {
+       data = JSON.parse( user);
        data["user4"] = user["user4"];
-       console.log( data );
-       res.end( JSON.stringify(data));
+
+      var file = JSON.stringify(data);
+
+ fs.writeFileSync('addUser.json',file); 
+ res.send(file);
    });
 })
 
@@ -37,4 +29,3 @@ var server = app.listen(8081, function () {
   console.log("Example app listening at http://%s:%s", host, port)
 
 })
- 
